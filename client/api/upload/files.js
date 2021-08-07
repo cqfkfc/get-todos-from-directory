a6c2ad611c0d6bf5d2b11fe0034a93c5cf319d6f
cqfkfc <chuaqinfang@gmail.com>
2021-08-07 17:47:17 +0800

    test vercel post

--- client/api/upload/files.js
+++ client/api/upload/files.js
@@ -1,4 +1,30 @@
-module.exports = (req, res) => {
-  const { name = "World" } = req.query;
-  res.status(200).send(`Hello ${name}!`);
+const formidable = require("formidable");
+const fs = require("fs");
+
+module.exports = async (req, res) => {
+  const form = formidable({ multiple: true });
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    const promises = Object.values(files).map((file) => {
+      return new Promise((resolve, reject) => {
+        fs.readFile(file.path, function (error, data) {
+          if (error) throw error;
+          if (data.includes("TODO")) {
+            resolve(file.name);
+          } else {
+            resolve();
+          }
+          resolve(file.name, data.includes("TODO"));
+        });
+      });
+    });
+
+    Promise.all(promises).then((data) =>
+      res.send({ data: data.filter((item) => item !== undefined) })
+    );
+  });
 };
